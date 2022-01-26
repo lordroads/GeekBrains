5b9c07101ea39b3060e5a8e4e132834c68a2062b
Yurii Mitrohin <lordroads@mail.ru>
2022-01-26 12:50:22 +0300

    Решение задачи 1

--- lessond-2/Task-1/Program.cs
+++ lessond-2/Task-1/Program.cs
@@ -10,6 +10,28 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            /*
+            Задача 1
+
+            Запросить у пользователя минимальную и максимальную температуру за сутки
+            и вывести среднесуточную температуру.
+            */
+            int dailyTemp = 0;
+
+            Console.WriteLine("Введите минимальную температуру за сегодня");
+            string userTempMin = Console.ReadLine();
+            Console.WriteLine("Введите максимальную температуру за сегодня");
+            string userTempMax = Console.ReadLine();
+
+            if (int.TryParse(userTempMin, out int tempMin) && int.TryParse(userTempMax, out int tempMax))   //проверка на корректность ввода от пользователя
+            {
+                dailyTemp = (tempMax + tempMin) / 2;                                                        //подсчет средней температуры
+                Console.WriteLine($"Среднесуточную температура за сегодня: {dailyTemp}t");
+            }
+            else
+            {
+                Console.WriteLine("Ошиблись при вводе температуры");                                        //вывод сообщения об ошибке, если пользователь ввел не числа
+            }
         }
     }
 }
