b663e5b9e617d72432382c472fd97033d9415044
Yurii Mitrohin <lordroads@mail.ru>
2022-02-03 14:05:48 +0300

    Решение задачи 4

--- Lessond-3/Task-4/Program.cs
+++ Lessond-3/Task-4/Program.cs
@@ -6,7 +6,41 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * 
+             * Задание 4
+             * 
+             * (*) «Морской бой» — вывести на экран массив 10х10, 
+             * состоящий из символов X и O, 
+             * где Х — элементы кораблей, 
+             * а О — свободные клетки.           
+             *
+             */
+
+            //Массив 10х10
+            char[,] battleField =
+            {
+                { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
+                { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
+                { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
+                { 'X', 'O', 'O', 'X', 'X', 'X', 'X', 'O', 'O', 'X'},
+                { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'},
+                { 'O', 'O', 'X', 'O', 'O', 'O', 'O', 'X', 'O', 'O'},
+                { 'X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O', 'X'}
+            };
+
+            for (int x = 0; x <= battleField.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= battleField.GetUpperBound(1); y++)
+                {
+                    Console.Write($"\t{battleField[x, y]}");            //Вывод на консоль поочередно каждый символ.
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
