af5d3d3fda4b61c51061b2274c921fa3ffa84af1
Yurii Mitrohin <lordroads@mail.ru>
2022-02-03 14:03:07 +0300

    Решение задачи 3

--- Lessond-3/Task-3/Program.cs
+++ Lessond-3/Task-3/Program.cs
@@ -6,7 +6,33 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * 
+             * Задание 3
+             * 
+             * Написать программу, 
+             * выводящую введенную пользователем строку в обратном порядке 
+             * (olleH вместо Hello).            
+             *
+             */
+
+            //Получаем строку от пользователя
+            Console.WriteLine("Введите слово: ");
+            string userEnter = Console.ReadLine();
+            //можно проверить точно ли нам пользователь что то отправил
+            if (!string.IsNullOrEmpty(userEnter))                               //Если строка не пустая и не null то получим false
+            {
+                string newString = "";                                          //создаем новую строку для разворота слова от пользователя
+                for (int i = userEnter.Length - 1; i >= 0; i--)                 //проходимся по строке от пользователя в обратном порядке и сохраняем символы в новой строке
+                {
+                    newString += userEnter[i];
+                }
+                Console.WriteLine($"\"{newString}\" вместо \"{userEnter}\"");   //выводим новую строку на консоль
+            }
+            else
+            {
+                Console.WriteLine("Забыли ввести слово!");
+            }
         }
     }
 }
