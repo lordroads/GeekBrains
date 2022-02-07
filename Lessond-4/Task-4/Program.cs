9f65f176ba65173e750970e4d79d2dd9d3d0dbf2
Yurii Mitrohin <lordroads@mail.ru>
2022-02-07 22:54:52 +0300

    Решение задачи 4

--- Lessond-4/Task-4/Program.cs
+++ Lessond-4/Task-4/Program.cs
@@ -4,9 +4,44 @@ namespace Task_4
 {
     internal class Program
     {
+        /*
+        * Задача 4
+        * 
+        *  (*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом.
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите число для вычесление числа Фибоначчи.");
+            string userEnter = Console.ReadLine();
+            if (int.TryParse(userEnter, out int number))
+            {
+                double numFibonachi = Fibonachi(number);
+                Console.WriteLine($"Число Фибоначчи для числа {userEnter} - {numFibonachi}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число.");
+            }
+        }
+
+        //Формула чисел Фибоначчи: Fn = Fn-1 + Fn-2;
+        //F0 = 0;
+        //F1 = 1;
+        //F2 = F1 + F0 = 1;
+        //F3 = F2 + F1 = 2 и т.д
+        static double Fibonachi(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+
+            return Fibonachi(number - 1) + Fibonachi(number - 2);
         }
     }
 }
