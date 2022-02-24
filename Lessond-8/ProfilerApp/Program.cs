3e1e470f86353041e1f8da42869892df28199083
Yurii Mitrohin <lordroads@mail.ru>
2022-02-24 23:48:10 +0300

    Создание решения для урока 8 и создание проектов для домашних задач.

---
+++ Lessond-8/ProfilerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProfilerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
